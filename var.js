3c0ba48a7776e69274cdb1b8cbbbd1f0b19db63c
LotteKildebro <4514756@RTS-365.dk>
2018-01-19 11:01:34 +0100

    immiter

---
+++ var.js
@@ -0,0 +1,46 @@
+// Variable Øvelser
+
+// 2
+// let navn = "kage";
+// console.log(navn);
+
+
+// 3
+// let alder = 24;
+// console.log(alder);
+
+// 4 (n ska være stort)
+// let byNavn = "Roskilde";
+// console.log(byNavn);
+
+// 5
+// let x = 40;
+// let y = 2;
+// console.log(x + y);
+
+//let x = 5;
+//let y = 10;
+//let z = x+y;
+//console.log(z);
+
+//let mitFornavn = 'Lotte';
+//let mitEfternavn = 'Kildebro';
+//let minAlder = '24';
+//console.log('Hej, jeg hedder ' + mitFornavn + ' ' + mitEfternavn+ ' og jeg er ' + minAlder + ' år');
+
+//let moms = 1.25;
+//let prisUdenMoms = 400;
+//let prisMedMoms = prisUdenMoms*moms;
+//let prisUdenMoms2 = prisMedMoms/moms;
+//console.log(prisUdenMoms2);
+
+//let forste = 10;
+//let anden = 20;
+
+
+//let kage = anden;
+//anden = forste;
+//forste = kage;
+//console.log(forste,anden);
+
+
